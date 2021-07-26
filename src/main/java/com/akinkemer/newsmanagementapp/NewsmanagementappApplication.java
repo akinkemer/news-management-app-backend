8ab2d631d9b34134e9a683093237bcbed3522895
Akın Kemer <kemerakin@gmail.com>
2021-07-26 20:18:58 +0300

    Initial commit

---
+++ src/main/java/com/akinkemer/newsmanagementapp/NewsmanagementappApplication.java
@@ -0,0 +1,13 @@
+package com.akinkemer.newsmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsmanagementappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsmanagementappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akinkemer/newsmanagementapp/NewsmanagementappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akinkemer.newsmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsmanagementappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
